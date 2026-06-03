2bef4cbde78792b61299642acf23742ec7250951
Thomas Seeling <thomas.seeling@gmx.net>
2026-06-03 16:15:50 +0200

    add upgrade script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fedora-scripts
 Collection of useful scripts for Fedora maintenance
+
+## fc-update.sh
+
+This script will update a mixed environment where Fedora coexists with Linux from Scratch,
+and `grub.cfg` is under control of LFS, not Fedora. It contains code to update the kernel and initramfs versions.
+
+## upgrade-f44.sh
+
+This script will upgrade a bunch of machines to the next Fedora version.
+It tries to minimize downloads by loading and storing the upgrade file from and to a NAS before the actual upgrade.
+TBD: eliminate duplicates if there are new package releases between upgrading multiple machines.
---
+++ upgrade-f44.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+dont=
+me="${0##*-f}"
+me="${me%.sh}"
+me="${1:-${me}}"
+
+me="${me:-44}"
+nasdir="/nas/image/linux/Fedora/f${me}"
+nasdirdnf="${nasdir}/libdnf5"
+libdnf=/usr/lib/sysimage
+
+echo "# upgrade to Fedora ${me}"
+journalctl --vacuum-time 4h
+
+${dont} dnf -y clean all
+${dont} dnf -y upgrade --refresh
+${dont} dnf -y install dnf-plugin-system-upgrade
+
+ssh root@nas mkdir -p "${nasdirdnf}"
+echo "# get cache files from ${nasdir}"
+rsync -rva root@nas:"${nasdirdnf}" "${libdnf}"
+
+${dont} dnf -y system-upgrade download --refresh --releasever="${me}"
+${dont} rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-"${me}"-primary
+
+scp /etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-"${me}"-primary root@nas:"${nasdir}"
+rsync -rva "${libdnf}" root@nas:"${nasdir}"
+
+echo dnf5 -y offline reboot
